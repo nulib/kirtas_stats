69e0c8049e67c957c7e0d4d32babf7b61bc1816a
paclough <paclough@github>
2013-06-07 02:24:17 -0400

    Initial simple models working. Can get open jobs and parse the job info (as a hash).

--- stats_ar.rb
+++ stats_ar.rb
@@ -7,14 +7,53 @@ config = YAML::load(
 ActiveRecord::Base.establish_connection(
     config["development"])
 
-class JBPM_TASKINSTANCE < ActiveRecord::Base
-	self.table_name = "JBPM_TASKINSTANCE"
-
-	def get_open_jobs
-		JBPM_TASKINSTANCE.where( END_: nil )
-	end
+class Task < ActiveRecord::Base
+    self.table_name = "JBPM_TASKINSTANCE"
 end
 
-tasks = JBPM_TASKINSTANCE.new
-open_jobs = tasks.get_open_jobs()
-open_jobs.each { |job| puts job.PROCINST_ }
+class ProcessInstance < ActiveRecord::Base
+  self.table_name = "JBPM_PROCESSINSTANCE"
+
+  def get_open_jobs
+    ProcessInstance.where( "END_ is null and PROCESSDEFINITION_ > 10" )
+  end
+end
+
+class JobInfo < ActiveRecord::Base
+  self.table_name = "JBPM_VARIABLEINSTANCE"
+
+  def get_job_info( job_num )
+    JobInfo.where( "PROCESSINSTANCE_ = ?", job_num )
+  end
+end
+
+def get_job_info_hash( job_info )
+  job_info_hash = {}
+  job_info.each { |job| job_info_hash = { job.STRINGVALUE_ => job.NAME_ } }
+  p job_info_hash
+end
+
+
+jobs = ProcessInstance.new
+job_info = JobInfo.new
+
+open_jobs = jobs.get_open_jobs()
+puts open_jobs.count
+
+open_jobs.each do |job|
+  a_job = job_info.get_job_info( job.ID_ )
+  a_job_hash = {}
+  a_job.each { |job| a_job_hash[ job.NAME_ ] = job.STRINGVALUE_ }
+  p a_job_hash[ "electronic_bib_id" ]
+  p a_job_hash[ "title" ]
+end
+
+
+
+# open_jobs.each do |job|
+#   job_num = job.ID_
+#   # job_title = job_info.get_job_info( job_num )
+#   # job_info.get_job_info( job_num ).each { |info| p job_num.to_s + " - " + info.STRINGVALUE_ if info.NAME_ == "title" }
+#   p get_job_info_hash( get_job_info( job_num ) )
+# end
+# p job_info.get_job_info( open_jobs.first.ID_ ).last.NAME_
