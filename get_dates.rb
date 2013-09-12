0114d3d3ebb757bfce364f74d9057beb0571e132
Paul Clough <paclough@github>
2013-09-12 09:48:56 -0500

    Clean up get_dates.rb by updating period_start_end to use the previous month directly.

--- get_dates.rb
+++ get_dates.rb
@@ -16,18 +16,12 @@ module GetDates
     return fiscal_start, fiscal_end
   end
 
-  def self.prev_month_start_end
-    prev_month        = Date.today.prev_month
-    prev_month_start  = Date.new( prev_month.year, prev_month.month, 1 ).strftime( "%Y-%m-%d" )
-    prev_month_end    = Date.new( prev_month.year, prev_month.month, -1 ).strftime( "%Y-%m-%d" )
-    return prev_month_start, prev_month_end
-  end
-
   def self.period_start_end
-    period_start  = prev_month_start_end.first
-    period_end    = prev_month_start_end.last
+    prev_month        = Date.today.prev_month
+    period_start  = Date.new( prev_month.year, prev_month.month, 1 ).strftime( "%Y-%m-%d" )
+    period_end    = Date.new( prev_month.year, prev_month.month, -1 ).strftime( "%Y-%m-%d" )
 
     puts "The current period is #{period_start} to #{period_end}."
-    return period_start, period_endl
+    return period_start, period_end
   end
 end
