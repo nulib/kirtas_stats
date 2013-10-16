99de05010098dfcd69dd060ce2bebcc446132ba8
Paul Clough <paclough@github>
2013-10-16 11:49:39 -0500

    Added quarterly_start_end method
    In order to add the new stats (yearly/quarterly/monthly) we need to add some
    helper methods to figure out which year/quarter/month we're in and when the
    beginning and end of that period are. Quarters were missing previously, so I've
    added them here.

--- get_dates.rb
+++ get_dates.rb
@@ -1,6 +1,7 @@
 require 'date'
 
 module GetDates
+
   def self.fiscal_start_end
 
     current = Date.today
@@ -16,6 +17,48 @@ module GetDates
     return fiscal_start, fiscal_end
   end
 
+  def self.quarterly_start_end
+
+    today = Date.today
+
+    if today < Date.new( today.year, 9, 1 ) then
+      q1_start = Date.new( today.prev_year.year, 9, 1 )
+      q1_end   = Date.new( today.prev_year.year, 11, -1 )
+      q2_start = Date.new( today.prev_year.year, 12, 1 )
+      q2_end   = Date.new( today.year, 2, -1 )
+      q3_start = Date.new( today.year, 3, 1 )
+      q3_end   = Date.new( today.year, 5, -1 )
+      q4_start = Date.new( today.year, 6, 1 )
+      q4_end   = Date.new( today.year, 8, -1 )
+    else
+      q1_start = Date.new( today.year, 9, 1 )
+      q1_end   = Date.new( today.year, 11, -1 )
+      q2_start = Date.new( today.year, 12, 1 )
+      q2_end   = Date.new( today.next_year.year, 2, -1 )
+      q3_start = Date.new( today.next_year.year, 3, 1 )
+      q3_end   = Date.new( today.next_year.year, 5, -1 )
+      q4_start = Date.new( today.next_year.year, 6, 1 )
+      q4_end   = Date.new( today.next_year.year, 8, -1 )
+    end
+
+    q1 = ( q1_start..q1_end )
+    q2 = ( q2_start..q2_end )
+    q3 = ( q3_start..q3_end )
+    q4 = ( q4_start..q4_end )
+
+    if q1.include?( today )
+      return [ q1_start, q1_end ]
+    elsif q2.include?( today )
+      return [ q2_start, q2_end ]
+    elsif q3.include?( today )
+      return [ q3_start, q3_end ]
+    else
+      return [ q4_start, q4_end ]
+    end
+
+  end
+
+
   def self.period_start_end
     today         = Date.today
     period_start  = Date.new( today.year, today.month, 1 ).strftime( "%Y-%m-%d" )
@@ -24,4 +67,5 @@ module GetDates
     puts "The current period is #{period_start} to #{period_end}."
     return period_start, period_end
   end
+
 end
