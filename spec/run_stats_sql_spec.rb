03e87ad4809098e31b04a9299968907e2c4d67cb
Paul Clough <paclough@github>
2013-10-27 09:38:30 -0500

    Adding remote MySQL functionality
    Adding tests and code to verify that the MySQL database on Repository is
    accessible and responds to a sample command ("show tables;"). The
    "sample_output" value comes from running the command manually and cutting-
    and-pasting the result.

--- spec/run_stats_sql_spec.rb
+++ spec/run_stats_sql_spec.rb
@@ -10,6 +10,10 @@ class RunStatsSQL
   def run_mysql_test
     `mysql >& mysql_out`
   end
+
+  def run_mysql_test_connect
+    `mysql -h #{ @config[ "host" ] } -u #{ @config[ "username" ] } -p#{ @config[ "password" ] } #{ @config[ "database" ] } -e "show tables;" >& mysql_out`
+  end
 end
 
 describe RunStatsSQL do
@@ -40,5 +44,58 @@ describe RunStatsSQL do
     File.delete( f )
 
     expect( file_exist ).to be_true
+  end
+
+  it "connects to the MySQL database on Repository" do
+    sample_output = %w[
+      Tables_in_jbpmdb
+      HILOSEQUENCES
+      JBPM_ACTION
+      JBPM_BYTEARRAY
+      JBPM_BYTEBLOCK
+      JBPM_COMMENT
+      JBPM_DECISIONCONDITIONS
+      JBPM_DELEGATION
+      JBPM_EVENT
+      JBPM_EXCEPTIONHANDLER
+      JBPM_ID_GROUP
+      JBPM_ID_MEMBERSHIP
+      JBPM_ID_PERMISSIONS
+      JBPM_ID_USER
+      JBPM_JOB
+      JBPM_LOG
+      JBPM_MODULEDEFINITION
+      JBPM_MODULEINSTANCE
+      JBPM_NODE
+      JBPM_POOLEDACTOR
+      JBPM_PROCESSDEFINITION
+      JBPM_PROCESSINSTANCE
+      JBPM_RUNTIMEACTION
+      JBPM_SWIMLANE
+      JBPM_SWIMLANEINSTANCE
+      JBPM_TASK
+      JBPM_TASKACTORPOOL
+      JBPM_TASKCONTROLLER
+      JBPM_TASKINSTANCE
+      JBPM_TOKEN
+      JBPM_TOKENVARIABLEMAP
+      JBPM_TRANSITION
+      JBPM_VARIABLEACCESS
+      JBPM_VARIABLEINSTANCE
+      JMS_MESSAGES
+      JMS_ROLES
+      JMS_SUBSCRIPTIONS
+      JMS_TRANSACTIONS
+      JMS_USERS
+      TIMERS ]
+
+    c = RunStatsSQL.new
+    c.run_mysql_test_connect
+    f = File.open( 'mysql_out' )
+    output = f.readlines.each { |line| line.chomp! }
+    f.close
+    File.delete( f )
+
+    expect( output ).to eql( sample_output )
   end
 end
